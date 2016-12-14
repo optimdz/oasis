0971b362accae16e151f3098777ae5fbc82c7631
LARABA MOHAMMED SEDDIK <lmseddik@icloud.com>
2016-12-14 22:48:32 +0100

    First project + module

---
+++ oasis.core/src/main/java/com/touggourti/oasis/Tools.java
@@ -0,0 +1,12 @@
+package com.touggourti.oasis;
+
+/**
+ * Created by seddik on 14/12/2016.
+ */
+
+public class Tools {
+    public static String getCreators()
+    {
+        return "Abdelouahed + Mohammed Seddik";
+    }
+}
